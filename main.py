06137ea6b6e5701918f7d45332869d2eeac9e9f3
RandumTom <kramertom964@gmail.com>
2026-03-23 22:38:46 -0400

    Added a "memorie" function that allows the assistant to remember the name the user wants to be called using a json file

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import json
+import os
 from art import tprint
 
-print("Hello! What do you want me to call you?")
-name = input()
+CONFIG_FILE = "config.json"
+
+def load_name():
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, "r") as f:
+            data = json.load(f)
+            return data.get("name")
+    return None
+
+def save_name(name):
+    with open(CONFIG_FILE, "w") as f:
+        json.dump({"name": name}, f)
+
+name = load_name()
+
+if name is None:
+    print("Hello! What do you want me to call you?")
+    name = input()
+    save_name(name)
+
 tprint(f"Hello, {name}!", font="tarty1")
