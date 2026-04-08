7b9c6363e4ded1021a63ef4d5c68c468a93d9c03
RandumTom <kramertom964@gmail.com>
2026-04-08 19:29:32 -0400

    Update gui.py

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,3 +1,9 @@
 import sys
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
 
+app = QGuiApplication(sys.argv)
+
+engine = QQmlApplicationEngine()
+
+engine.load("GUI/main.qml")
